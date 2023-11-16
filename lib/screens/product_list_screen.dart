41773dd75381ca185b4684c960e4f362218ec451
fabioychinen <astoufo_chinen@hotmail.com>
2023-11-16 12:21:32 -0300

    correct commit excluding duplicated itens

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:storage_test/models/product.dart';
-import 'package:storage_test/repositories/product_repository.dart';
+//import 'package:storage_test/repositories/product_repository.dart';
 import 'package:storage_test/screens/product_detail_screen.dart';
 import 'package:storage_test/blocs/product_bloc.dart';
 import 'package:storage_test/blocs/product_events.dart';
@@ -19,65 +19,64 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   late ProductBloc _bloc;
-  late List<Product> productlist = [];
 
   @override
   void initState() {
     super.initState();
-    _bloc = ProductBloc(productRepository: ProductRepository());
-    _bloc.add(LoadProductEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ProductBloc(productRepository: ProductRepository()),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Lista de Produtos',
-            style: TextStyle(
-                fontFamily: 'RussoOne',
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-          ),
+    _bloc = BlocProvider.of<ProductBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Lista de Produtos',
+          style: TextStyle(
+              fontFamily: 'RussoOne',
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
-          child: BlocBuilder<ProductBloc, ProductState>(
-              bloc: _bloc,
-              builder: (context, state) {
-                if (state is ProductInitialState) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state is ProductSuccessState) {
-                  final productList = state.products;
-                  return ListView.separated(
-                    itemCount: productList.length,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text(productList[index].name),
-                      trailing: IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            _bloc.add(RemoveProductEvent(
-                                productId: productList[index].id!));
-                          }),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProductDetailScreen(
-                                product: productList[index]),
-                          ),
-                        );
-                      },
-                    ),
-                    separatorBuilder: (_, __) => const Divider(),
-                  );
-                }
-                return Container();
-              }),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: BlocBuilder<ProductBloc, ProductState>(
+          bloc: _bloc,
+          builder: (context, state) {
+            if (state is ProductInitialState) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is ProductSuccessState) {
+              final productList = state.products;
+              return ListView.separated(
+                itemCount: productList.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(productList[index].name),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      _bloc.add(RemoveProductEvent(
+                          productId: productList[index].id!));
+                    },
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductDetailScreen(
+                          product: productList[index],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                separatorBuilder: (_, __) => const Divider(),
+              );
+            }
+            return Container();
+          },
         ),
       ),
     );
